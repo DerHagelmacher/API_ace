4851ee2391c3a32fe5ae9577a27968b8ce274069
Nicolas Ammeter <nicolas.ammeter@hotmail.com>
2025-09-25 15:45:04 +0200

    Fixes an aufgabe 1 und kleines rein HTML frontend auf /api/ui

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from . import temp, prime, number
 
 app = FastAPI()
-
 app.include_router(temp.router, prefix="/api")
 app.include_router(prime.router, prefix="/api")
 app.include_router(number.router, prefix="/api")
 
+app.mount("/api/ui", StaticFiles(directory="app/static", html=True), name="ui")
--- app/temp.py
+++ app/temp.py
@@ -5,7 +5,14 @@ router = APIRouter()
 @router.get("/temp")
 async def temp(celsius: float | None = None, kelvin: float | None = None):
     if (celsius is None and kelvin is None) or (celsius is not None and kelvin is not None):
-        raise HTTPException(400, "Nur einen Parameter angeben")
+        raise HTTPException(400, "Nur kelvin oder celsius angeben")
     if celsius is not None:
-        return {"celsius": celsius, "kelvin": celsius + 273.15}
-    return {"kelvin": kelvin, "celsius": kelvin - 273.15}
+        kelvin = celsius + 273.15
+        if kelvin < 0:
+            raise HTTPException(400, "Temperatur darf nicht unter 0 Kelvin liegen")
+        return {"celsius": celsius, "kelvin": kelvin}
+    celsius = kelvin - 273.15
+    if kelvin < 0:
+        raise HTTPException(400, "Temperatur darf nicht unter 0 Kelvin liegen")
+    return {"kelvin": kelvin, "celsius": celsius}
+
